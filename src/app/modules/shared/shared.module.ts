e43c659efc1c1b4437284d40aaa1cf7c479f3e09
Misiek40k <comanderrr@gmail.com>
2021-01-04 20:41:29 +0100

    Add Section-title component

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SectionTitleComponent } from './section-title/section-title.component';
 
 @NgModule({
-    declarations: [],
-    imports: [],
+    declarations: [SectionTitleComponent],
+    imports: [
+        CommonModule
+    ],
     exports: [
         CommonModule,
+        SectionTitleComponent,
     ]
 })
 export class SharedModule {}
