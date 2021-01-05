a3df56bc573f1320bd9d83f0593ed3115c2f5fe4
Misiek40k <comanderrr@gmail.com>
2021-01-06 00:17:42 +0100

    Add slider logic

--- src/app/modules/main-page/hero/hero.component.ts
+++ src/app/modules/main-page/hero/hero.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
 import { ImageData } from '../../shared/content-box/content-box.component';
+import { PageScrollInstance, PageScrollService } from 'ngx-page-scroll-core';
+import { DOCUMENT } from '@angular/common';
+
+enum ScrollDirection {
+    LEFT,
+    RIGHT
+}
 
 @Component({
     selector: 'app-hero',
@@ -8,13 +15,53 @@ import { ImageData } from '../../shared/content-box/content-box.component';
 })
 export class HeroComponent implements OnInit {
 
+    ScrollDirection = ScrollDirection;
+
+    @ViewChild('scrollContainer') scrollContainer: ElementRef;
+    @ViewChild('scrollButtonLeft') scrollButtonLeft: ElementRef;
+    @ViewChild('scrollButtonRight') scrollButtonRight: ElementRef;
+    currentSliderScrollIndex = 0;
+
     imageData: ImageData = {
         src: 'assets/images/Shape-1.png',
         srcSet: 'assets/images/Shape-1.png 1x, assets/images/Shape-1@2x.png 2x',
         alt: 'Shape'
     };
 
-    constructor() { }
+    constructor(
+        private pageScrollService: PageScrollService,
+        @Inject(DOCUMENT) private document: any,
+    ) { }
 
     ngOnInit(): void {}
+
+
+
+    scrollSlider(direction: ScrollDirection): void {
+        switch (direction) {
+            case ScrollDirection.LEFT:
+                if (this.currentSliderScrollIndex > 1) {
+                    this.currentSliderScrollIndex -= 1;
+                }
+                break;
+            case ScrollDirection.RIGHT:
+                if (this.currentSliderScrollIndex < 3) {
+                    this.currentSliderScrollIndex += 1;
+                }
+                break;
+        }
+        this.scroll(this.scrollContainer.nativeElement, 400, this.currentSliderScrollIndex, 'scrollItem');
+    }
+
+    private scroll(container, duration: number, index: number, targetId: string): void {
+        const pageScrollInstance: PageScrollInstance = this.pageScrollService.create({
+            document: this.document,
+            scrollTarget: '#' + targetId + '-' + index,
+            scrollViews: [container],
+            duration,
+            scrollOffset: 25,
+            verticalScrolling: false,
+        });
+        this.pageScrollService.start(pageScrollInstance);
+    }
 }
